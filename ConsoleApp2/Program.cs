a1fa86d371775e16aac8c7817955d50f96d09002
marcus lucious <marcus.j.lucious@outlook.com>
2018-08-14 08:50:18 -0400

    added a MD file

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine
+                ("Hello World!");
+
             Console.ReadKey();
         }
     }
---
+++ ConsoleApp2/readme.md.txt
@@ -0,0 +1,5 @@
+# My great test project
+
+Here is some text describing what this project does. 
+
+https://www.google.com/search?q=car&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjcubzHyezcAhVSn-AKHehEBl0Q_AUICigB&biw=1536&bih=787#imgrc=yA9yLp2u0qj0JM:
